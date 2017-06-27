745bcf57801473f464c63dac609a303e91dd2d6c
Ilya Fateev <fat32nov@gmail.com>
2017-06-27 11:43:12 +0300

    Log

--- get_hastag.py
+++ get_hastag.py
@@ -23,19 +23,24 @@ def build_wall_url(owner_id, post_id):
 def build_message():
     response_text = bot_util.urlopen(_NEWSFEED_SEARCH_URL)
     if not response_text:
+        print "Failed to get data from VK"
         return None
     response_json = json.loads(response_text)
     if 'response' not in response_json:
+        print "No 'response' in response"
         return None
     response = response_json['response']
     if 'items' not in response:
+        print "No 'items' in 'response'"
         return None
     items = response['items']
     if len(items) == 0:
+        print "Length of 'items' is zero"
         return None
     last_item = items[0]
     if 'owner_id' not in last_item and 'id' not in last_item:
-       return None
+        print "No 'owner_id' and 'id' in item"
+        return None
     last_item_url = build_wall_url(last_item['owner_id'], last_item['id'])
     last_item_in_file = bot_util.read_one_string_file(_LAST_ITEM_FILENAME)
     if not last_item_in_file or last_item_url != last_item_in_file:
